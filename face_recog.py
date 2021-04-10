a01f39b427c30017d09b6e3ef11ef6c52574b808
liam-robbins <lrobb001@ucr.edu>
2021-04-10 16:29:01 -0700

    testing

--- face_recog.py
+++ face_recog.py
@@ -45,24 +45,23 @@ while True:
 	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
-	
+	if not frame_number % 10:
 		
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30))
-	# OpenCV returns bounding box coordinates in (x, y, w, h) order
-	# but we need them in (top, right, bottom, left) order, so we
-	# need to do a bit of reordering
-	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
-	# loop over the facial embeddings
-	if not frame_number % 100:
+		# convert the input frame from (1) BGR to grayscale (for face
+		# detection) and (2) from BGR to RGB (for face recognition)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		# detect faces in the grayscale frame
+		rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+			minNeighbors=5, minSize=(30, 30))
+		# OpenCV returns bounding box coordinates in (x, y, w, h) order
+		# but we need them in (top, right, bottom, left) order, so we
+		# need to do a bit of reordering
+		boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+		# compute the facial embeddings for each face bounding box
+		encodings = face_recognition.face_encodings(rgb, boxes)
+		names = []
+			# loop over the facial embeddings
 		for encoding in encodings:
 			# attempt to match each face in the input image to our known
 			# encodings
@@ -91,13 +90,13 @@ while True:
         
 
         	# loop over the recognized faces
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		# draw the predicted face name on the image
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 0), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			0.75, (0, 255, 0), 2)
+		for ((top, right, bottom, left), name) in zip(boxes, names):
+			# draw the predicted face name on the image
+			cv2.rectangle(frame, (left, top), (right, bottom),
+				(0, 255, 0), 2)
+			y = top - 15 if top - 15 > 15 else top + 15
+			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+				0.75, (0, 255, 0), 2)
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF

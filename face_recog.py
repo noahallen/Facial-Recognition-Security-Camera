1de10c1eba95369a546d79805db8ed618a60abd1
liam-robbins <lrobb001@ucr.edu>
2021-04-10 16:26:36 -0700

    testing

--- face_recog.py
+++ face_recog.py
@@ -90,13 +90,13 @@ while True:
         
 
         	# loop over the recognized faces
-		for ((top, right, bottom, left), name) in zip(boxes, names):
-			# draw the predicted face name on the image
-			cv2.rectangle(frame, (left, top), (right, bottom),
-				(0, 255, 0), 2)
-			y = top - 15 if top - 15 > 15 else top + 15
-			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-				0.75, (0, 255, 0), 2)
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF

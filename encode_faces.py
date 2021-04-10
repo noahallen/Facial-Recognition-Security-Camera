9020bccd14a96f20f865c7a914f8e69aa6d99f1e
SahilPatelCA <sahil.patel.ca@hotmail.com>
2021-04-10 15:52:27 -0700

    Trained the datasets with our photos

--- encode_faces.py
+++ encode_faces.py
@@ -1,3 +1,9 @@
+# USAGE
+# When encoding on laptop, desktop, or GPU (slower, more accurate):
+# python encode_faces.py --dataset dataset --encodings encodings.pickle --detection-method cnn
+# When encoding on Raspberry Pi (faster, more accurate):
+# python encode_faces.py --dataset dataset --encodings encodings.pickle --detection-method hog
+
 # import the necessary packages
 from imutils import paths
 import face_recognition
@@ -5,6 +11,7 @@ import argparse
 import pickle
 import cv2
 import os
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", required=True,
@@ -18,6 +25,7 @@ args = vars(ap.parse_args())
 # grab the paths to the input images in our dataset
 print("[INFO] quantifying faces...")
 imagePaths = list(paths.list_images(args["dataset"]))
+
 # initialize the list of known encodings and known names
 knownEncodings = []
 knownNames = []
@@ -28,16 +36,20 @@ for (i, imagePath) in enumerate(imagePaths):
 	print("[INFO] processing image {}/{}".format(i + 1,
 		len(imagePaths)))
 	name = imagePath.split(os.path.sep)[-2]
-	# load the input image and convert it from BGR (OpenCV ordering)
+
+	# load the input image and convert it from RGB (OpenCV ordering)
 	# to dlib ordering (RGB)
 	image = cv2.imread(imagePath)
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 	# detect the (x, y)-coordinates of the bounding boxes
 	# corresponding to each face in the input image
 	boxes = face_recognition.face_locations(rgb,
 		model=args["detection_method"])
+
 	# compute the facial embedding for the face
 	encodings = face_recognition.face_encodings(rgb, boxes)
+
 	# loop over the encodings
 	for encoding in encodings:
 		# add each encoding + name to our set of known names and
--- face_recog.py
+++ face_recog.py
@@ -1,3 +1,6 @@
+# USAGE
+# python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
@@ -7,6 +10,7 @@ import imutils
 import pickle
 import time
 import cv2
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--cascade", required=True,
@@ -20,15 +24,17 @@ args = vars(ap.parse_args())
 print("[INFO] loading encodings + face detector...")
 data = pickle.loads(open(args["encodings"], "rb").read())
 detector = cv2.CascadeClassifier(args["cascade"])
+
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
+
 # start the FPS counter
 fps = FPS().start()
 
- #loop over frames from the video file stream
+# loop over frames from the video file stream
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to 500px (to speedup processing)
@@ -39,23 +45,29 @@ while True:
 	# detection) and (2) from BGR to RGB (for face recognition)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
 	# detect faces in the grayscale frame
 	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30))
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
 	# OpenCV returns bounding box coordinates in (x, y, w, h) order
 	# but we need them in (top, right, bottom, left) order, so we
 	# need to do a bit of reordering
 	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
 	# compute the facial embeddings for each face bounding box
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
-    	# loop over the facial embeddings
+
+	# loop over the facial embeddings
 	for encoding in encodings:
 		# attempt to match each face in the input image to our known
 		# encodings
 		matches = face_recognition.compare_faces(data["encodings"],
 			encoding)
 		name = "Unknown"
+
 		# check to see if we have found a match
 		if True in matches:
 			# find the indexes of all matched faces then initialize a
@@ -63,11 +75,13 @@ while True:
 			# was matched
 			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
 			counts = {}
+
 			# loop over the matched indexes and maintain a count for
 			# each recognized face face
 			for i in matchedIdxs:
 				name = data["names"][i]
 				counts[name] = counts.get(name, 0) + 1
+
 			# determine the recognized face with the largest number
 			# of votes (note: in the event of an unlikely tie Python
 			# will select first entry in the dictionary)
@@ -75,9 +89,8 @@ while True:
 		
 		# update the list of names
 		names.append(name)
-        
 
-        	# loop over the recognized faces
+	# loop over the recognized faces
 	for ((top, right, bottom, left), name) in zip(boxes, names):
 		# draw the predicted face name on the image
 		cv2.rectangle(frame, (left, top), (right, bottom),
@@ -85,19 +98,23 @@ while True:
 		y = top - 15 if top - 15 > 15 else top + 15
 		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 			0.75, (0, 255, 0), 2)
+
 	# display the image to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
+
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
+
 	# update the FPS counter
 	fps.update()
-    
+
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()

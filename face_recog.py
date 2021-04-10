017afa0a2d3830280329148836c64b46c540937e
liam-robbins <lrobb001@ucr.edu>
2021-04-10 16:05:55 -0700

    testing

--- face_recog.py
+++ face_recog.py
@@ -34,7 +34,7 @@ while True:
 	# to 500px (to speedup processing)
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
-	if frame_number % 10: continue
+	
 	# convert the input frame from (1) BGR to grayscale (for face
 	# detection) and (2) from BGR to RGB (for face recognition)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -50,31 +50,32 @@ while True:
 	encodings = face_recognition.face_encodings(rgb, boxes)
 	names = []
     	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown"
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
-		
-		# update the list of names
-		names.append(name)
+	if False:
+		for encoding in encodings:
+			# attempt to match each face in the input image to our known
+			# encodings
+			matches = face_recognition.compare_faces(data["encodings"],
+				encoding)
+			name = "Unknown"
+			# check to see if we have found a match
+			if True in matches:
+				# find the indexes of all matched faces then initialize a
+				# dictionary to count the total number of times each face
+				# was matched
+				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+				counts = {}
+				# loop over the matched indexes and maintain a count for
+				# each recognized face face
+				for i in matchedIdxs:
+					name = data["names"][i]
+					counts[name] = counts.get(name, 0) + 1
+				# determine the recognized face with the largest number
+				# of votes (note: in the event of an unlikely tie Python
+				# will select first entry in the dictionary)
+				name = max(counts, key=counts.get)
+			
+			# update the list of names
+			names.append(name)
         
 
         	# loop over the recognized faces

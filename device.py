0b0eba1fd9218e126be725be1a11c46d379b0d2e
liam-robbins <lrobb001@ucr.edu>
2021-04-11 05:13:03 -0700

    added ultrasonic

--- device.py
+++ device.py
@@ -43,8 +43,8 @@ def distance():
 if __name__ == '__main__':
     try:
         while True:
-            # dist = distance()
-            print ("Measured Distance = %.1f cm" % 1)
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
  
         # Reset by pressing CTRL + C

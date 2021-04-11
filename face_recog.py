c7cf5c9040c142a5e7bddc8c2b800a84bf65257f
liam-robbins <lrobb001@ucr.edu>
2021-04-10 22:48:54 -0700

    enhanced filename

--- face_recog.py
+++ face_recog.py
@@ -99,7 +99,9 @@ while True:
 				prev_name[name] = 0
 				date = str(datetime.now())[:-10]
 				raw = date.split(' ')
-				date = date.join(raw)
+				s='-'
+				date = s.join(raw)
+
 				cv2.imwrite("img/{}-{}.png".format(date,name), frame)
 				
 				time = time
